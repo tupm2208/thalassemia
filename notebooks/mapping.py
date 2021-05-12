3a5c61c9cb26b80d36fce67149a445c5b7ae7507
tupm <tupm2208@gmail.com>
2021-05-12 23:21:15 +0700

    update

---
+++ notebooks/mapping.py
@@ -0,0 +1,85 @@
+feature_names = ['750HbA1', '751HbA2', '752HbE', '753HbF']
+
+alpha_names = ['SEA', 'A32431', '3.7', 'A32428', '4.2', 'A32426', 'THAI', 'A32432', 'FIL', 'HbCS', 'A32427', 'HbQs', 'A32441', '2delT', 'A32429']
+
+beta_names = ['-28', 'cd 27/28', 'CD28', 'cd15', 'cd 17', 'CD17', 'A32435', 'Co17', 'cd 19', 'cd 8/9', 'cd 26', 'CD26', 'Co26', 'A32433', '41/42', 'A32442', 'cd 43', '71/72', 'cd 95', 'CD95', 'cd121', 'IVS', 'A32440', '89/90', 'cd90', '-88', 'b-29', 'b-31']
+
+basic_names = ['PID', '223SLHC', '224HST', '225HCT', '226MCV', '227MCH', '228MCHC', '233RDW-CV', '234SLTC', '246SLBC']
+# basic_names = ['PID', '223SLHC', '224HST', '225HCT', '226MCV', '227MCH', '228MCHC', '236', '234SLTC', '246SLBC']
+fe_names = ['1012FE', '1075SAT']
+
+
+
+mp = {
+    "PID": "PID",
+    "223SLHC": "SLHC",
+    "224HST": "HST",
+    "225HCT": "HCT",
+    "226MCV": "MCV",
+    "227MCH": "MCH",
+    "228MCHC": "MCHC",
+    "233RDW-CV": "RDWCV",
+    # "236": "RDWCV",
+    "234SLTC": "SLTC",
+    "246SLBC": "SLBC",
+    "1075SAT": "FE",
+    "1012FE": "FERRITIN",
+    "750HbA1": "HBA1",
+    "751HbA2": "HBA2",
+    "752HbE": "HBE",
+    "753HbF": "HBF",
+
+ 
+
+    "SEA": "SEA",
+    "A32431": "SEA",
+    "3.7": "A37",
+    "A32428": "A37",
+    "4.2": "A42",
+    "A32426": "A42",
+    "THAI": "THAI",
+    "A32432": "THAI",
+    "FIL": "FIL",
+    "HbCS": "HBCS",
+    "A32427": "HBCS",
+    "HbQs": "HBQS",
+    "A32441": "HBQS",
+    "2delT": "C2DELT",
+    "A32429": "C2DELT",
+    'a2 cd': 'a2cd',
+    'a1 cd': 'a1cd',
+    
+    "-28": "28",
+    "cd 27/28": "28",
+    "CD28": "28",
+    "cd15": "CD15",
+    "cd 17": "CD17",
+    "CD17": "CD17",
+    "A32435": "CD17",
+    "Co17": "CD17",
+    "cd 19": "CD19",
+    "cd 8/9": "CD8.9",
+    "cd 26": "CD26",
+    "CD26": "CD26",
+    "Co26": "CD26",
+    "A32433": "CD26",
+    "41/42": "CD41.42",
+    "A32442": "CD41.42",
+    "cd 43": "CD43",
+    "71/72": "CD71.72",
+    "cd 95": "CD95",
+    "CD95": "CD95",
+    "cd121": "CD121",
+    "IVS": "IVS1.1",
+    "A32440": "IVS2.654",
+    "89/90": "90",
+    "cd90": "90",
+    "-88": "88",
+    "b-29": "29",
+    "b-31": "31",
+    'cd ': 'cd',
+    'codon': 'codon',
+    'thal -90': "-90"
+}
+
+pattern = {e: None for e in mp.keys()}
---
+++ src/processing/convert2.py
@@ -0,0 +1,123 @@
+from os import name
+import pandas as pd
+from tqdm import tqdm
+
+path = "datasets/pid.csv"
+df = pd.read_csv(path)
+flag_dict = {e.strip(): True for e in df['PID']}
+columns = ['intime', 'PID', 'SID', 'Age', 'sex', 'Address', 'LocationID', 'LocationName', 'Testcode', 'Testname', 'Result']
+pattern = None
+
+def process_line(source_path, des_path):
+    f2 = open(des_path, 'w+', encoding='utf8')
+    with open(source_path, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        
+        out = []
+        for line in tqdm(lines):
+            splited = line.split(',')
+            out = splited[:5] + ['-'.join(splited[5:-5])] + splited[-5:]
+            
+            f2.write(','.join(out))
+    f2.close()
+    print("file converted!!!")
+
+
+def process_csv(source_path):
+    global pattern
+    with open(source_path, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+
+        arr = []
+        for line in tqdm(lines):
+            line = line.strip()
+            out = line.split(',')
+            PID = out[1].strip()
+            out[1] = PID
+            if PID is not None and flag_dict.get(PID):
+                arr.append(out)
+    
+    df = pd.DataFrame(arr, columns=columns)
+    # df.set_index('PID', inplace=True)
+    df = df.assign(Testcode=df.Testcode.str.strip())
+    pattern = {e: None for e in df.Testcode.unique()}
+    return df
+
+def handle_group(ex_df):
+    global pattern
+    obj = pattern.copy()
+    df2 = ex_df[['intime', 'Testcode', 'Result']]
+    df2 = df2.assign(indate=df2['intime'].str.split().str[0])
+    df2.apply(pd.to_numeric, errors='coerce', downcast='float')
+    # df2.dropna(inplace=True)
+    m = 0
+    selected_df = None
+    for date, sub_df in df2.groupby('indate'):
+        if m <= sub_df.shape[0]:
+            m = sub_df.shape[0]
+            obj['indate'] = date
+            selected_df = sub_df
+
+    main_df = selected_df[['Testcode','Result']]
+    main_dict = main_df.set_index('Testcode').to_dict()['Result']
+    obj.update(main_dict)
+    return obj
+
+def process_dataframe(df, des_path):
+    out_arr = []
+    for pid, sub_df in tqdm(df.groupby("PID")):
+        pid = str(int(pid)).strip()
+        if pid is not None and flag_dict.get(pid):
+            tgt = handle_group(sub_df)
+            tgt['PID'] = pid
+            out_arr.append(tgt)
+    
+    tgt_df = pd.DataFrame(out_arr).set_index("PID")
+    tgt_df.to_csv(des_path)
+
+def handle_xlsx(file_path, des_folder):
+    global pattern
+    # xl = pd.ExcelFile(file_path)
+    # for e in tqdm(xl.sheet_names):
+    for e in ['SH-2013', 'SH-2015', 'SH-2016', 'SH-2017', 'SH-2018', 'SH-2019', 'SH-2020']:
+        des_path =  f'{des_folder}/{e}.csv'
+        csv_path = f'{os.path.dirname(file_path)}/{e}.csv'
+        if os.path.isfile(csv_path):
+            df = pd.read_csv(csv_path)
+        else:
+            df = xl.parse(e)
+            df.to_csv(csv_path, index=False)
+            pass
+        pattern = {e: None for e in df.Testcode.unique()}
+        process_dataframe(df, des_path)
+
+def main(source_path, des_path):
+    if source_path.endswith('.csv'):
+        df = process_csv(source_path)
+        process_dataframe(df, des_path)
+    elif source_path.endswith('.xlsx'):
+        handle_xlsx(source_path, des_path)
+
+
+
+if __name__ == '__main__':
+    from glob import glob
+    import os
+
+    # file_paths = glob('datasets/OneDrive_1_5-10-2021/4. XN_PCR_Di truyền sinh học phân tử/*')
+    # for path in file_paths:
+    #     print(path)
+    #     inte_path = path.replace(".csv", '_modify.csv')
+    #     des_path = f'datasets/dien_di/{os.path.basename(path)}'
+    #     process_line(path, inte_path)
+    #     main(inte_path, des_path)
+
+    # path = 'datasets/OneDrive_1_5-10-2021/2. XN_Sinh hóa máu/SINH HOA-VHH2013-2019.xlsx'
+    # des_path = 'datasets/SINH_HOA'
+    # main(path, des_path)
+
+    path = 'datasets/OneDrive_1_5-10-2021/1. XN_CBC_Tổng phân tích tế bào máu/TB.2015.csv'
+    inte_path = path.replace(".csv", '_modify.csv')
+    des_path = 'datasets/TBM/TB.2015.csv'
+    process_line(path, inte_path)
+    main(inte_path, des_path)
