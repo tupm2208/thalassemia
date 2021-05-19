c2bed078b6132f8baee65d17ce23432926e90f6c
tupm <tupm2208@gmail.com>
2021-05-19 09:45:51 +0700

    update

--- src/semi_train.py
+++ src/semi_train.py
@@ -14,7 +14,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 
-# numerical_columns = ["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC"]
+numerical_columns = ["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC"]
 # numerical_columns = ["FERRITIN","FE"]
 numerical_columns = ["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC","FERRITIN","FE"]
 # numerical_columns = ["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC","FERRITIN","FE", "HBA1", "HBA2"]
@@ -27,15 +27,24 @@ def read_main_data(file_path = 'datasets/splited_train_test.csv'):
 
     df = pd.read_csv(file_path, low_memory=False)
     df.drop_duplicates(subset=['PID'], keep='last', inplace=True)
+    # df = df[~(df.sheet.isin(['d1']) & (df.thalas == 0)& (df.train == 1))]
+    # df1 = df[~df.sheet2.isin(['normal'])]
+    # df2 = df[df.sheet2.isin(['normal'])]
+    # df = pd.concat([df1, df2.iloc[:2000]])
+    # df = pd.concat([df1, df2])
+    # df = df1
+    # print(df1.shape, df2.shape)
+
     # df = df[((df.MCV<85) | (df.MCH<28))]
     # df.set_index("PID", inplace=True)
     
-    # num_df = df[numerical_columns].apply(pd.to_numeric, errors='coerce', downcast='float')
-    num_df = df[["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC","FERRITIN","FE"]].apply(pd.to_numeric, errors='coerce', downcast='float')
+    num_df = df[numerical_columns].apply(pd.to_numeric, errors='coerce', downcast='float')
+    # num_df = df[["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC","FERRITIN","FE"]].apply(pd.to_numeric, errors='coerce', downcast='float')
     
     df = df.reindex(num_df.dropna().index)
 
-    df[["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC","FERRITIN","FE"]] = num_df.dropna()
+    # df[["SLHC","HST","HCT","MCV","MCH","MCHC","RDWCV","SLTC","SLBC","FERRITIN","FE"]] = num_df.dropna()
+    df[numerical_columns] = num_df.dropna()
 
     # train_df, test_df = train_test_split(df[numerical_columns + ['thalas']], test_size=0.2, stratify=df.thalas, random_state=42)
 
@@ -86,9 +95,13 @@ def balance_data(df, type='under'):
     p_size = positive_df.shape[0]
     n_size = negative_df.shape[0]
 
-    sz = min(p_size, n_size)
-    if type == 'over':
+    
+    if type == 'under':
+        sz = min(p_size, n_size)
+    elif type == 'over':
         sz = max(p_size, n_size)
+    else:
+        sz = type
     if n_size != sz:
         s = np.random.randint(n_size, size=sz)
         negative_df = negative_df.loc[s].reset_index(drop=True)
@@ -108,7 +121,7 @@ def evaluate(model, df, desc=''):
     preds = np.array(model.predict(X))
     print('---------------------------------------------------')
     print(f"evaluation results {desc}:\n")
-    print(classification_report(y, np.where(preds>=0.5, 1, 0)))
+    print(classification_report(y, np.where(preds>=0.6, 1, 0)))
     print('---------------------------------------------------')
 
     # mask = preds != y
@@ -117,7 +130,7 @@ def evaluate(model, df, desc=''):
 
     features_df = df[mask][numerical_columns].reset_index(drop=True)
     labels = preds[mask]
-    labels = np.where(labels>=0.5, 1, 0)
+    labels = np.where(labels>=0.8, 1, 0)
     # print(labels)
     new_df = pd.concat([features_df, pd.DataFrame({'thalas': labels})], axis=1)
 
@@ -127,7 +140,9 @@ def evaluate(model, df, desc=''):
 def fit_model(model, df):
     global numerical_columns
 
+    # df = balance_data(df, 5000)
     # df = balance_data(df, 'under')
+    # df = balance_data(df, 'over')
     X = process_features(df)
     y = df['thalas']
 
@@ -140,32 +155,33 @@ def fit_model(model, df):
 def main():
     train_df, test_df = read_main_data()
 
-    model = xgb.XGBClassifier(learning_rate=0.1, max_depth=12, n_estimators=100, n_jobs=-1, random_state=42,use_label_encoder=False, objective="binary:logistic")
+    # model = xgb.XGBClassifier(learning_rate=0.1, max_depth=15, n_estimators=100, n_jobs=-1, random_state=42,use_label_encoder=False, objective="binary:logistic")
+    model = xgb.XGBRegressor(learning_rate=0.1, max_depth=15, n_estimators=100, n_jobs=-1, random_state=42,use_label_encoder=False, objective="binary:logistic")
     # model = RandomForestClassifier(n_estimators = 100, random_state=42, criterion='entropy', n_jobs=8)
     # model = GaussianNB()
     # model = RandomForestRegressor(n_estimators = 100, random_state=42,n_jobs=8)
     # weighted = train_df.shape[0] / (2 * np.bincount(train_df.thalas))
-    # model = svm.SVC(C=0.5,kernel='rbf', class_weight={0: weighted[0], 1: weighted[1]})
+    # model = svm.SVC(C=0.1,kernel='rbf', class_weight={0: weighted[0], 1: weighted[1]})
     # model = svm.SVC(C=10,kernel='rbf', class_weight={0: weighted[0], 1: weighted[1]})
     # model = svm.SVC(C=200,kernel='rbf')
-    # model = KNeighborsClassifier(n_neighbors=3)
+    # model = KNeighborsClassifier(n_neighbors=4)
 
     # svm_model = svm.SVC(C=200,kernel='rbf', probability=True)
-    xg = xgb.XGBClassifier(learning_rate=0.1, max_depth=15, n_estimators=100, n_jobs=-1, random_state=42)
-    rf = RandomForestClassifier(n_estimators = 100, random_state=42, criterion='entropy', n_jobs=8)
+    # xg = xgb.XGBClassifier(learning_rate=0.1, max_depth=15, n_estimators=100, n_jobs=-1, random_state=42)
+    # rf = RandomForestClassifier(n_estimators = 100, random_state=42, criterion='entropy', n_jobs=8)
     # nb = GaussianNB()
     # model = VotingClassifier(estimators=[('rf', rf), ('nb', nb), ('xg', xg)], voting='soft')
     # model = VotingClassifier(estimators=[('rf', rf), ('xg', xg)], voting='soft')
 
     
-    # df2 = read_additional_data('datasets/0504.csv')
-    # df3 = read_additional_data('datasets/1904.csv')
-
     # test_df = pd.concat([test_df, df3])
     # df2 = pd.concat([df2, df3])
     fit_model(model, train_df)
     evaluate(model, test_df, 'main_test')
     evaluate(model, train_df, 'main_test')
+    import pickle
+    with open('models/model_fe.pkl', 'wb') as f:
+        pickle.dump(model, f)
 
 if __name__ == '__main__':
     # read_additional_data('datasets/0504.csv')
